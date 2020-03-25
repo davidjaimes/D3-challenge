905aad0261d59c4ea31921b045cbe6f348c55cdd
David Jaimes <david@djaimes.com>
2020-03-24 18:54:00 -0700

    collect poverty and state abbreviations

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-// @TODO: YOUR CODE HERE!
+// Load data from hours-of-tv-watched.csv
+d3.csv("./assets/data/data.csv").then(function(data) {
+
+  // Collect abbreviations and poverty data points.
+  var abbr = data.map(d => d.abbr);
+  var poverty = data.map(d => d.poverty);
+
+  // Load x and y data points
+
+
+}).catch(function(error) {
+  console.log(error);
+});
