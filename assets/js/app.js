dc765632d757a29679e0721a739b1de9c7dd028c
David Jaimes <david@djaimes.com>
2020-03-24 19:25:33 -0700

    convert strings to integers

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
-// Load data from hours-of-tv-watched.csv
+// Load data from assets/data/data.csv
 d3.csv("./assets/data/data.csv").then(function(data) {
 
-  // Collect abbreviations and poverty data points.
+  // Collect state abbreviations and poverty data points
+  // and convert to strings to integers.
   var abbr = data.map(d => d.abbr);
-  var poverty = data.map(d => d.poverty);
+  var poverty = data.map(d => +d.poverty);
+  var health = data.map(d => +d.healthcare);
 
   // Load x and y data points
 
