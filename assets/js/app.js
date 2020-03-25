cc0501aa19521078f4b6bf4e50deb334d9f0c120
David Jaimes <david@djaimes.com>
2020-03-24 19:42:10 -0700

    add axes and circles

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,65 @@
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 // Load data from assets/data/data.csv
 d3.csv("./assets/data/data.csv").then(function(data) {
 
-  // Collect state abbreviations and poverty data points
-  // and convert to strings to integers.
-  var abbr = data.map(d => d.abbr);
-  var poverty = data.map(d => +d.poverty);
-  var health = data.map(d => +d.healthcare);
+  // Convert to strings to integers.
+  data.forEach(d => {
+    d.poverty = +d.poverty;
+    d.healthcare = +d.healthcare
+  });
 
-  // Load x and y data points
+  // Create x and y scale functions.
+  var xLinearScale = d3.scaleLinear()
+    .domain([8, d3.max(data, d => d.poverty)])
+    .range([0, width]);
 
+  var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.healthcare)])
+    .range([height, 0]);
+
+  // Create axis functions.
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // Append axes to chart.
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // Create circles in chart.
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "10")
+    .attr("fill", "lightblue")
+    .attr("opacity", ".75");
 
 }).catch(function(error) {
   console.log(error);
